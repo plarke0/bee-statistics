6dcfad43ebb20f6078c8501bc5eac9d2e6c380e5
plarke0 <plarke@byu.edu>
2026-06-19 23:27:55 -0700

    Initial commit

---
+++ src/main/java/net/plarke/bee_statistics/BeeStatisticsClient.java
@@ -0,0 +1,10 @@
+package net.plarke.bee_statistics;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BeeStatisticsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
